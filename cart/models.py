bb4582c14d96eba3e5cb2c5d36fe59c22a8eec06
Erfan Motallebi <fs.c0d3rz@gmail.com>
2025-04-01 04:17:44 -0700

    Feat: add cart models

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+from store.models import Product
+
+
+
 # Create your models here.
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=250, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__( self ):
+        return self.cart_id
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__( self ):
+        return self.product
