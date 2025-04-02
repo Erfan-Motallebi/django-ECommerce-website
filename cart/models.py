cb0236757c9691fff912097d54c045ae45bf0c46
Erfan Motallebi <fs.c0d3rz@gmail.com>
2025-04-02 19:32:40 +0330

    PKG: install packages CONFIG: do the migrations

--- cart/models.py
+++ cart/models.py
@@ -3,13 +3,12 @@ from django.db import models
 from store.models import Product
 
 
-
 # Create your models here.
 class Cart(models.Model):
     cart_id = models.CharField(max_length=250, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
-    def __str__( self ):
+    def __str__(self):
         return self.cart_id
 
 
@@ -19,5 +18,5 @@ class CartItem(models.Model):
     quantity = models.PositiveIntegerField()
     is_active = models.BooleanField(default=True)
 
-    def __str__( self ):
+    def __str__(self):
         return self.product
