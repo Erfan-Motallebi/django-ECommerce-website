4ab88081a235b372ff1adbb60a2ff84f84a055c2
Erfan Motallebi <fs.c0d3rz@gmail.com>
2025-04-03 18:19:47 +0330

    Feat: Paginator + Search functionalities

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+
 from . import views
+
 urlpatterns = [
-    path('', view = views.store, name='store'),
-    path('<slug:category_slug>/', view = views.store, name='products_by_category'),
-    path('<slug:category_slug>/<slug:product_slug>/', view = views.product_detail, name='product_detail'),
+    path('', view=views.store, name='store'),
+    path('category/<slug:category_slug>/', view=views.store, name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>/', view=views.product_detail, name='product_detail'),
+    path('search/', view=views.search, name='search'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,5 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404
 
@@ -12,13 +14,19 @@ def store(request: HttpRequest, category_slug: str = None) -> HttpResponse:
     if category_slug is not None:
         category = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=category, is_available=True)
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count,
     }
     return render(request=request, template_name="store/store.html", context=context)
@@ -34,3 +42,25 @@ def product_detail(request: HttpRequest, category_slug: str, product_slug: str) 
         'in_cart': in_cart,
     }
     return render(request=request, template_name="store/product_detail.html", context=context)
+
+
+def search(request: HttpRequest) -> HttpResponse:
+    products = {}
+    product_count = 0
+
+    if request.GET["keyword"]:
+        keyword = request.GET["keyword"]
+        products = Product.objects.filter(Q(product_name__icontains=keyword) | Q(description__icontains=keyword))
+    else:
+        products = Product.objects.all().filter(is_available=True)
+
+    paginator = Paginator(products, 6)
+    page = request.GET.get('page')
+    paged_products = paginator.get_page(page)
+    product_count = products.count()
+
+    context = {
+        'products': paged_products,
+        'product_count': product_count,
+    }
+    return render(request=request, template_name="store/store.html", context=context)
